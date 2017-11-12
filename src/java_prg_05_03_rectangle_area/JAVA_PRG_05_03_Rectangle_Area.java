5d022ab06a0e095b2326efe3f5c01f9b1b408722
Keith Smith <32456218+afewvowels@users.noreply.github.com>
2017-11-12 13:31:40 -0500

    Prompt written.

---
+++ src/java_prg_05_03_rectangle_area/JAVA_PRG_05_03_Rectangle_Area.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java_prg_05_03_rectangle_area;
+
+/**
+ *
+ * @author bluebackdev
+ * If you have downloaded the book's source code from www.pearsonhighered.com/gaddis,
+ * you will find a partially written program named AreaRectangle.java in the
+ * chapter's source code folder. Your job is to complete the program. When it is
+ * complete, the program will ask the user to enter the width and length of a
+ * rectangle, and then display the rectangle's area. The program calls the
+ * following methods, which have not been written:
+ * 
+ *      getLength - This method should ask the user to enter the rectangle's
+ *      length, and then return that value as a double.
+ *      getWidth - This method should ask the user to enter the rectangle's
+ *      width, and then return that value as a double.
+ *      getArea - This method should accept the rectangle's length and width
+ *      as arguments, and return the rectangle's area. The area is calculated
+ *      by multiplying the length by the width.
+ *      displayData - This method should accept the rectangle's length, width,
+ *      and area as arguments, and display theml in an appropriate message
+ *      on the screen.
+ */
+public class JAVA_PRG_05_03_Rectangle_Area {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
